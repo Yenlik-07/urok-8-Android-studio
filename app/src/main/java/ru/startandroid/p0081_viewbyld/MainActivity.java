61d6128499efc5a93d1766f815b94599c6361903
Your Name <you@example.com>
2020-11-15 16:14:56 +0600

    first commit

---
+++ app/src/main/java/ru/startandroid/p0081_viewbyld/MainActivity.java
@@ -0,0 +1,26 @@
+package ru.startandroid.p0081_viewbyld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView myTextView = (TextView) findViewById(R.id.myText);
+        myTextView.setText("Android it's easy");
+        Button myBtn = (Button) findViewById(R.id.myBtn);
+        myBtn.setText("My button");
+        myBtn.setEnabled(false);
+
+        CheckBox myChb = (CheckBox) findViewById(R.id.myChb);
+        myChb.setChecked(true);
+    }
+}
